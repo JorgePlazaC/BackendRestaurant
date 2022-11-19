3ae58c9b3557ba97c3a92daf91d8f2487d15a41f
JorgePlazaC <chocheplaza@hotmail.com>
2022-11-18 21:26:10 -0300

    Imagen modificada

--- database/migrations/2022_10_05_001409_create_productos_table.php
+++ database/migrations/2022_10_05_001409_create_productos_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -18,13 +19,14 @@ return new class extends Migration
             $table->string('nombre');
             $table->unsignedBigInteger('idCategoria');
             $table->foreign('idCategoria')->references('id')->on('categorias')->onDelete("cascade");
-            $table->binary('imagen')->nullable();
             $table->string('descripcion')->nullable();
             $table->integer('precio')->nullable();
             $table->integer('stock')->nullable();
             $table->integer('cant')->nullable();
             $table->timestamps();
         });
+
+        DB::statement("ALTER TABLE productos ADD imagen LONGBLOB");
     }
 
     /**
